3e0a4da266f5a810d74ca3ea0ed06f8ce1e283f3
Rodolfo Oliveira <47088607+dorfusouza@users.noreply.github.com>
2024-09-03 16:19:54 -0300

    atualização

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.addEventListener('scroll', function() {
+    const scrollPosition = window.pageYOffset;
+    const homeSection = document.querySelector('.home');
+    
+    // Ajusta o valor do divisor para controlar a velocidade do efeito
+    homeSection.style.backgroundPositionY = `${(scrollPosition * 0.5)}px`;
+});
 
